399d9e1ee32efe1df3754842efa8bad13612f4ed
p3lim <adrianlund@gmail.com>
2009-02-15 01:27:36 +0100

    Added DebuffHighlight support Minimal cleanup

--- oUF_Mini.lua
+++ oUF_Mini.lua
@@ -1,32 +1,6 @@
---[[
-local function HasDType(unit, t)
-	for i = 1, 40 do
-		local name, _, _, _, dtype = UnitDebuff(unit, i)
-		if(not name) then
-			return
-		elseif(dtype == t) then
-			return true
-		end
-	end
-end
-
-local function UpdateBackdrop(self)
-	for i = 1, 40 do
-		local name, _, _, _, dtype = UnitDebuff(self.unit, i)
-		if(not name and (dtype == 'Poison' or dtype == 'Curse')) then
-			local color = DebuffTypeColor[dtype]
-			self:SetBackdropColor(color.r, color.g, color.b)
-		else
-			self:SetBackdropColor(0, 0, 0)
-		end
-	end
-end
---]]
-local function OverrideUpdateHealth(self, event, unit, bar)
-	local localized, english = UnitClass(unit)
-	local color = self.colors.class[english]
-	if(color) then bar.bg:SetVertexColor(unpack(color)) end
-	self:SetAlpha(UnitIsDeadOrGhost(unit) and 0.25 or 1)
+local function ColorBackground(self)
+	local localized, class = UnitClass(self.unit)
+	self.Health.bg:SetVertexColor(unpack(self.colors.class[class] or self.colors.health))
 end
 
 local function CreateStyle(self, unit)
@@ -48,14 +22,17 @@ local function CreateStyle(self, unit)
 	self.Health.bg = self.Health:CreateTexture(nil, 'BACKGROUND')
 	self.Health.bg:SetAllPoints(self.Health)
 	self.Health.bg:SetTexture([=[Interface\ChatFrame\ChatFrameBackground]=])
+	table.insert(self.__elements, ColorBackground)
 
 	self.ReadyCheck = self.Health:CreateTexture(nil, 'OVERLAY')
 	self.ReadyCheck:SetAllPoints(self.Health)
 
---	self:RegisterEvent('UNIT_AURA', UpdateBackdrop) -- needs to be fixed
+	self.DebuffHighlightBackdrop = true
+	self.DebuffHighlightFilter = true
 
-	self.OverrideUpdateHealth = OverrideUpdateHealth
-	self.disallowVehicleSwap = true
+	self.OverrideUpdateHealth = function(self)
+		self:SetAlpha((UnitIsDeadOrGhost(self.unit) or not UnitIsConnected(self.unit)) and 0.25 or 1)
+	end
 end
 
 oUF:RegisterStyle('Mini', CreateStyle)
